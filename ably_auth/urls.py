acb9d7f419af9c58a19a9a0fdc2afc9abd733dd5
kyunghan.k <kyunghan.k@turtleship.io>
2022-08-21 23:46:34 +0900

    feat: simple-jwt 를 통한 로그인 추가

--- ably_auth/urls.py
+++ ably_auth/urls.py
@@ -1,4 +1,8 @@
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from django.urls import path, include
 
@@ -9,4 +13,6 @@ router.register(r'phone_auth', phone_auth_view.PhoneAuthViewSet, basename='phone
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- mokoko/settings.py
+++ mokoko/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_simplejwt',
     'ably_auth'
 ]
 
@@ -125,6 +126,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
 
 CURRENT_LANGUAGE = "kor"
 I18N = importlib.import_module(f'mokoko.i18n.{CURRENT_LANGUAGE}').language_module()
