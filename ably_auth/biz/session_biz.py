3dbd355a94aab915bf439a743f4733088fe0a0f9
kyunghan.k <kyunghan.k@turtleship.io>
2022-08-24 14:28:27 +0900

    fix: session 만료 시 예외처리

--- ably_auth/biz/session_biz.py
+++ ably_auth/biz/session_biz.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from typing import Type, TypeVar
 
 from django.contrib.sessions.models import Session
@@ -8,6 +9,9 @@ SessionType = TypeVar('SessionType', bound=Session)
 def validate_session(session_key: str) -> Type[SessionType] | None:
     try:
         session = Session.objects.get(session_key=session_key)
+        is_expired = session.expire_date < datetime.now(tz=timezone.utc)
+        if is_expired:
+            return None
         return session
     except Session.DoesNotExist:
         return None
